36a4b9c77db42a84eee921004aa8b69caf3e0e8d
Raghavendrapara <41910672+Raghavendrapara@users.noreply.github.com>
2021-08-15 13:41:52 +0530

    Update BookingController.java

--- src/main/java/com/team17/desking/controllers/BookingController.java
+++ src/main/java/com/team17/desking/controllers/BookingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.team17.desking.entity.Booking;
 import com.team17.desking.repository.BookingRepository;
@@ -17,18 +18,18 @@ public class BookingController {
 
 	@Autowired
 	private BookingRepository bookrepo;
-	
+	@CrossOrigin()
 	@GetMapping("/desking/booking/{userId}")	
 	public List<Object> getUserBookings(@PathVariable Long userId){
 		return bookrepo.getUserBookingsAndSeatInfo(userId);	
 	}
-	
+	@CrossOrigin()
 	@PostMapping("/desking/booking/create")
 	public Long add(@RequestBody Booking booking) {
 	    Booking newBooking = bookrepo.save(booking);
 	    return newBooking.getBookingId();
 	}
-	
+	@CrossOrigin()
 	@DeleteMapping("/desking/booking/{bookingId}")
 	public void delete(@PathVariable Long bookingId) {
 	    bookrepo.deleteById(bookingId);
